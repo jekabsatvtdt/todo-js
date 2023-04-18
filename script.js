d41c839d06174bd2524bc35b1f6ff3c11acd2f4a
jekabsatvtdt <116027757+jekabsatvtdt@users.noreply.github.com>
2023-04-18 16:34:27 +0300

    js risinājums

---
+++ script.js
@@ -0,0 +1,7 @@
+function add() {
+  const uzd = document.getElementById("ievade").value;
+  const listItem = document.createElement("li");
+  listItem.textContent = uzd;
+  document.getElementById("list").appendChild(listItem);
+  document.getElementById("ievade").value = "";
+}
